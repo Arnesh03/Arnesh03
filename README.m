60b74e06bc770ce785268b033089ba9d9beec1c8
Arnesh <98650760+Arnesh03@users.noreply.github.com>
2022-01-31 11:23:33 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
 - 👋 Hi, I’m @Arnesh03
-- 👀 I’m interested in projects  and beautiful experience of coding.
+- 👀 I’m interested in projects, hackathons, open source coding and many more.
 - 🌱 I’m currently learning web development.
 - 💞️ I’m looking to collaborate on git hub if requried.
 
